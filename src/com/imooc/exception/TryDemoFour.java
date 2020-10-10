200d75aab19e54c4af7f9e674c2ac0af9bc65aa6
tankwu <470285886@qq.com>
2020-10-10 18:16:30 +0800

    更新异常部分

--- src/com/imooc/exception/TryDemoFour.java
+++ src/com/imooc/exception/TryDemoFour.java
@@ -1,6 +1,7 @@
 package com.imooc.exception;
 
 import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TryDemoFour {
@@ -8,7 +9,13 @@ public class TryDemoFour {
     public static void main(String[] args) {
         try {
             testAge();
-        } catch (Throwable e) {
+        } catch (HotelAgeException e) {
+            /*e.printStackTrace();*/
+            System.out.println(e.getMessage());
+            System.out.println("酒店前台工作人员不允许办理入住登记");
+        }catch (InputMismatchException e){
+            System.out.println("格式错误");
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
@@ -34,13 +41,14 @@ public class TryDemoFour {
         }
     }*/
 
-    public static void testAge() throws Throwable {
+    public static void testAge() throws HotelAgeException {
 
             System.out.println("请输入年龄：");
             Scanner input = new Scanner(System.in);
             int age = input.nextInt();
             if (age < 18 || age > 80) {
-                throw new Exception("18岁以下，80岁以上的住客必须由亲友陪同");
+                //throw new Exception("18岁以下，80岁以上的住客必须由亲友陪同");
+                throw new HotelAgeException();
             } else {
                 System.out.println("欢迎入住本酒店");
             }
